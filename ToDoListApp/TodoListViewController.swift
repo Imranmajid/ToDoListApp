7c12c3849cabf5e0ed2ea54695377b155122dfea
Imranmajid <imranmajid@me.com>
2018-01-26 09:31:12 +0000

    All functions working, with addition button and label showing the number of to do items.

--- ToDoListApp/TodoListViewController.swift
+++ ToDoListApp/TodoListViewController.swift
@@ -10,7 +10,40 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let toDoListArray = ["Find House", "Arrange Viewing", "Market Current Property", "Arrange Move", "Complete School Forms"]
+    var toDoListArray = ["Find House", "Arrange Viewing", "Market Current Property", "Arrange Move", "Complete School Forms"]
+    
+    
+    @IBOutlet weak var numberOfItems: UIBarButtonItem!
+    
+    //MARK - 'Add' Button Function
+    
+    @IBAction func barAddButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var addedItem = UITextField()
+        
+        let alert   = UIAlertController(title: "To Do App", message: "", preferredStyle: .alert)
+        let action  = UIAlertAction(title: "Add", style: .default) { (action) in
+            if addedItem.text != "" {
+                self.toDoListArray.append(addedItem.text!)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (additionalItem) in
+            additionalItem.placeholder = "Enter item..."
+            addedItem = additionalItem
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func trashButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +52,9 @@ class TodoListViewController: UITableViewController {
     //MARK - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        numberOfItems.title = String(toDoListArray.count)
         return toDoListArray.count
+
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
